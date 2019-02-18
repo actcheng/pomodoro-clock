ace4b124e18c34007acc718c79c4fd2a471fb641
CT Cheng (Anna) <annacheng310@gmail.com>
2019-02-18 22:12:13 +0900

    Create the layout and basic JS structure. 13/29 tests passed

---
+++ app.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+});
+
+let body=document.body;
+let timeLeft_div = document.getElementById('time-left')
+let sessionLength_div = document.getElementById('session-length')
+let breakLength_div = document.getElementById('break-length')
+
+function main(){
+  $('#session-increment').click(function(){
+
+  });
+  $('#session-decrement').click(function(){
+
+  });
+  $('#break-increment').click(function(){
+
+  });
+  $('#break-decrement').click(function(){
+
+  });
+  $('#start_stop').click(function(){
+
+  });
+  $('#reset').click(function(){
+
+  });
+}
+
+main();
