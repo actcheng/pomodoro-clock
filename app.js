a3d74c598ab3f2c973372bcb885ca4d867e63d6c
CT Cheng (Anna) <annacheng310@gmail.com>
2019-02-20 21:37:37 +0900

    Updated data structure. Added long break option.

--- app.js
+++ app.js
@@ -2,53 +2,84 @@ $(document).ready(function() {
 
 });
 
-let body              = document.body;
+let settings = {
+  session: {
+    length_div : document.getElementById('session-length'),
+    lengthDefault: 25,
+    length: 25,
+    text: 'Productivity Time'
+  },
+  shortBreak: {
+    length_div : document.getElementById('sbreak-length'),
+    lengthDefault: 5,
+    length: 5,
+    text: 'Short Break'
+  },
+  longBreak: {
+    length_div : document.getElementById('lbreak-length'),
+    lengthDefault: 15,
+    length: 15,
+    text: 'Long Break'
+  },
+}
+
+let records = {
+  session: {
+    count: 0,
+    duration: 0
+  },
+  shortBreak: {
+    count: 0,
+    duration: 0
+  },
+  longBreak: {
+    count: 0,
+    duration: 0
+  }
+}
+
 let startStop_div     = document.getElementById('start_stop');
 let timerLabel_div    = document.getElementById('timer-label');
 let timeLeft_div      = document.getElementById('time-left');
-let sessionLength_div = document.getElementById('session-length');
-let breakLength_div   = document.getElementById('break-length');
 let beep_div          = document.getElementById('beep');
 
-let sessionLengthDefault = 25;
-let breakLengthDefault = 5;
-
-let sessionText = 'Session';
-let breakText = 'Break';
-
-let timerLabel = sessionText;
-let sessionLength = sessionLengthDefault;
-let breakLength = breakLengthDefault;
-let timeLeftMin = sessionLength;
-let timeLeftSec = 0;
+let timerType     = '';
+let timerLabel    = '';
+let timeLeftMin   = 0;
+let timeLeftSec   = 0;
 
 let counting = false;
-let playing  = false;
-let i = 1;
 
 var x = null;
 
 function countDown(){
-
-  counting = !counting
-  if (counting){
-    $(startStop_div).text('Stop');
-    x = setInterval(function(){
-      timeLeftSec -= 1;
-      if (timeLeftSec<0){
-        if (timeLeftMin>0){
-          timeLeftMin -= 1;
-          timeLeftSec += 60;
-        } else {
-          clearInterval(x);
-          timeUp();
-        }
-      }
-      changeTimeLeftText();
-    },1000);
+  if (timeLeftSec == 0 && timeLeftMin==0){
+    resetValue();
   } else {
-    $(startStop_div).text('Start');
-    clearInterval(x);
+    counting = !counting
+    beep_div.pause();
+    beep_div.currentTime = 0;
+
+    if (counting){
+      $(startStop_div).text('Stop');
+      x = setInterval(function(){
+        timeLeftSec -= 1;
+        if (timeLeftSec<0){
+          if (timeLeftMin>0){
+            timeLeftMin -= 1;
+            timeLeftSec += 60;
+          } else {
+            timeLeftSec = 0;
+            clearInterval(x);
+            timeUp();
+          }
+        }
+        changeTimeLeftText();
+      },10);
+    } else {
+      $(startStop_div).text('Start');
+      clearInterval(x);
+    }
   }
 }
 
@@ -57,45 +88,34 @@ function changeTimeLeftText(){
   $(timeLeft_div).text(timeLeft);
 }
 
+function updateRecord(timePassed){
+  records[timerType].count += 1;
+  records[timerType].duration += timePassed;
+  console.log(timerType,records[timerType])
+}
+
 function timeUp(){
   beep_div.play();
-  if (timerLabel===sessionText){
-    changeSession(breakText);
-  } else {
-    changeSession(sessionText);
-  }
-  counting = !counting;
-  countDown();
+  updateRecord(settings[timerType].length);
 }
 
 function changeSession(type){
-  if (type===sessionText){
-    timerLabel = sessionText;
-    timeLeftMin = sessionLength;
-    timeLeftSec = 0;
-    changeTimeLeftText();
-  } else { // break
-    timerLabel = breakText;
-    timeLeftMin = breakLength;
-    timeLeftSec = 0;
-    changeTimeLeftText();
+  if (counting){
+    countDown();
   }
+  timerLabel = settings[type].text;
+  timeLeftMin = settings[type].length;
+  timeLeftSec = 0;
+  changeTimeLeftText();
   $(timerLabel_div).text(timerLabel);
+  timerType = type;
 }
 
 function updateValue(type,increment){
-  if (type===sessionText){
-    sessionLength = Math.min(Math.max(sessionLength+increment,1),60);
-    $(sessionLength_div).text(sessionLength);
-    if (timerLabel==sessionText){
-      changeSession(type)
-    }
-  } else {
-    breakLength = Math.min(Math.max(breakLength+increment,1),60);
-    $(breakLength_div).text(breakLength);
-    if (timerLabel==breakText){
-      changeSession(type)
-    }
+  settings[type].length = Math.min(Math.max(settings[type].length+increment,1),60);
+  $(settings[type].length_div).text(settings[type].length);
+  if (timerType==type){
+    changeSession(type)
   }
 }
 
@@ -107,32 +127,33 @@ function resetValue(){
     $(startStop_div).text('Start');
     clearInterval(x);
   }
-  sessionLength = sessionLengthDefault;
-  breakLength = breakLengthDefault;
-  timeLeftMin = sessionLength;
+  timeLeftMin = settings[timerType].length;
   timeLeftSec = 0;
-
-  $(sessionLength_div).text(sessionLength);
-  $(breakLength_div).text(breakLength);
   changeTimeLeftText();
-  changeSession(sessionText);
 }
 
 function main(){
-  changeSession(sessionText);
-  updateValue(sessionText,0);
-  updateValue(breakText,0);
+  changeSession('session');
+  updateValue('session',0);
+  updateValue('shortBreak',0);
+  updateValue('longBreak',0);
   $('#session-increment').click(function(){
-    updateValue(sessionText,1);
+    updateValue('session',1);
   });
   $('#session-decrement').click(function(){
-    updateValue(sessionText,-1);
+    updateValue('session',-1);
   });
-  $('#break-increment').click(function(){
-    updateValue(breakText,1);
+  $('#sbreak-increment').click(function(){
+    updateValue('shortBreak',1);
   });
-  $('#break-decrement').click(function(){
-    updateValue(breakText,-1);
+  $('#sbreak-decrement').click(function(){
+    updateValue('shortBreak',-1);
+  });
+  $('#lbreak-increment').click(function(){
+    updateValue('longBreak',1);
+  });
+  $('#lbreak-decrement').click(function(){
+    updateValue('longBreak',-1);
   });
   $('#start_stop').click(function(){
     countDown();
@@ -141,10 +162,16 @@ function main(){
     resetValue();
   });
   $('#session-label').click(function(){
-    changeSession(sessionText);
+    changeSession('session');
+    countDown();
   });
-  $('#break-label').click(function(){
-    changeSession(breakText);
+  $('#sbreak-label').click(function(){
+    changeSession('shortBreak');
+    countDown();
+  });
+  $('#lbreak-label').click(function(){
+    changeSession('longBreak');
+    countDown();
   });
 }
 
