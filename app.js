82b164cb681dbd292ba3b39bfbbbd2921464e9c3
CT Cheng (Anna) <annacheng310@gmail.com>
2019-02-21 23:00:27 +0900

    Add overtime panel

--- app.js
+++ app.js
@@ -41,17 +41,31 @@ let records = {
 let startStop_div     = document.getElementById('start_stop');
 let timerLabel_div    = document.getElementById('timer-label');
 let timeLeft_div      = document.getElementById('time-left');
+let overtime_div      = document.getElementById('overtime');
+let overtimePanel_div = document.getElementById('overtime-panel');
 let beep_div          = document.getElementById('beep');
 
 let timerType     = '';
 let timerLabel    = '';
 let timeLeftMin   = 0;
 let timeLeftSec   = 0;
+let overtimeMin   = 0;
+let overtimeSec   = 0;
 
 let counting = false;
 
 var x = null;
 
+// Update settings
+function updateValue(type,increment){
+  settings[type].length = Math.min(Math.max(settings[type].length+increment,1),60);
+  $(settings[type].length_div).text(settings[type].length);
+  if (timerType==type){
+    changeSession(type)
+  }
+}
+
+// Counting
 function countDown(){
   if (timeLeftSec == 0 && timeLeftMin==0){
     resetValue();
@@ -59,6 +73,8 @@ function countDown(){
     counting = !counting
     beep_div.pause();
     beep_div.currentTime = 0;
+    clearOverTime();
+    $('#overtime-panel').hide();
 
     if (counting){
       $(startStop_div).text('Stop');
@@ -75,28 +91,30 @@ function countDown(){
           }
         }
         changeTimeLeftText();
-      },1000);
+      },1);
     } else {
-      $(startStop_div).text('Start');
-      clearInterval(x);
+      stopCounting();
     }
   }
 }
 
-function changeTimeLeftText(){
-  let timeLeft = ('0'+timeLeftMin).slice(-2)+':'+('0'+timeLeftSec).slice(-2)
-  $(timeLeft_div).text(timeLeft);
-}
-
-function updateRecord(timePassed){
-  records[timerType].count += 1;
-  records[timerType].duration += timePassed;
-  console.log(timerType,records[timerType])
+function countOvertime(){
+  $('#overtime-panel').fadeToggle(500);
+  $('#save-overtime').hide();
+  x = setInterval(function(){
+    overtimeSec += 1;
+    if (overtimeSec >60){
+      overtimeMin += 1;
+      overtimeSec -= 60;
+    }
+    changeOvertimeText();
+  },10);
 }
 
 function timeUp(){
   beep_div.play();
-  updateRecord(settings[timerType].length);
+  updateRecord(settings[timerType].length,timerType,true);
+  countOvertime();
 }
 
 function changeSession(type){
@@ -111,32 +129,66 @@ function changeSession(type){
   timerType = type;
 }
 
-function updateValue(type,increment){
-  settings[type].length = Math.min(Math.max(settings[type].length+increment,1),60);
-  $(settings[type].length_div).text(settings[type].length);
-  if (timerType==type){
-    changeSession(type)
+// Change texts
+function changeOvertimeText(){
+  let overtime = '+'+('0'+overtimeMin).slice(-2)+':'+('0'+overtimeSec).slice(-2)
+  $(overtime_div).text(overtime);
+}
+
+function changeTimeLeftText(){
+  let timeLeft = ('0'+timeLeftMin).slice(-2)+':'+('0'+timeLeftSec).slice(-2)
+  $(timeLeft_div).text(timeLeft);
+}
+
+// Update records
+function updateRecord(timePassedMin,type,addCount){
+  if (addCount){
+    records[type].count += 1;
   }
+  records[type].duration += timePassedMin
+  console.log(type,records[type])
+}
+
+function updateOvertimeRecord(timePassedMin,type){
+  updateRecord(overtimeMin,type,timerType!==type);
+  stopCounting();
+  $('#overtime-panel').fadeToggle(500);
+  setTimeout(function(){ clearOverTime() },500);
+}
+
+// Resetting
+function clearOverTime(){
+  overtimeMin = 0;
+  overtimeSec = 0;
+  changeOvertimeText();
 }
 
 function resetValue(){
   beep_div.pause();
   beep_div.currentTime = 0;
+  stopCounting();
+  changeSession(timerType);
+  //clearOverTime();
+}
+
+function stopCounting(){
+  if ($('#overtime-panel').is(':visible')){
+    $('#save-overtime').fadeToggle(500);
+  }
   if (counting){
     counting = !counting;
     $(startStop_div).text('Start');
     clearInterval(x);
   }
-  timeLeftMin = settings[timerType].length;
-  timeLeftSec = 0;
-  changeTimeLeftText();
 }
 
 function main(){
+  $('#overtime-panel').hide();
   changeSession('session');
   updateValue('session',0);
   updateValue('shortBreak',0);
   updateValue('longBreak',0);
+
   $('#session-increment').click(function(){
     updateValue('session',1);
   });
@@ -172,6 +224,15 @@ function main(){
   $('#lbreak-label').click(function(){
     changeSession('longBreak');
     countDown();
+  });
+  $('#to-session').click(function(){
+    updateOvertimeRecord(overtimeMin,'session');
+  });
+  $('#to-sbreak').click(function(){
+    updateOvertimeRecord(overtimeMin,'shortBreak');
+  });
+  $('#to-lbreak').click(function(){
+    updateOvertimeRecord(overtimeMin,'longBreak');
   });
 }
 
